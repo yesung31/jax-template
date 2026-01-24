280b1cfb373a1e3e9416c8c2e97f14ecfb0eddf5
Yesung Hwang <yesung.hw@gmail.com>
2026-01-24 16:53:42 +0100

    Auto import models and data modules

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,23 @@
-from .template.datamodule import TemplateDataModule
+import importlib
+import pkgutil
+
+from core.data import DataModule
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__, prefix=__name__ + "."):
+    if any(part.startswith(("_", "base")) for part in module_name.split(".")):
+        continue
+
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError:
+        continue
+
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+
+        if (
+            isinstance(attribute, type)
+            and issubclass(attribute, DataModule)
+            and attribute is not DataModule
+        ):
+            globals()[attribute_name] = attribute
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,17 @@
-from .template_model import TemplateModel
+import importlib
+import pkgutil
+
+from core.model import Model
+
+# Auto-import all Model children
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    if module_name in ["__init__", "base"]:
+        continue
+
+    module = importlib.import_module(f".{module_name}", package=__name__)
+
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+
+        if isinstance(attribute, type) and issubclass(attribute, Model) and attribute is not Model:
+            globals()[attribute_name] = attribute
